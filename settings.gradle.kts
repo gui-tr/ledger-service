3d05470052667fe865d51fde2892829ecfdc4f0d
gui-tr <gtrionfo.dev@pm.me>
2025-11-25 20:36:53 +0000

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+
+
+rootProject.name="ledger_v1"
+
---
+++ src/test/java/com/app/Ledger_v1Test.java
@@ -0,0 +1,21 @@
+package com.app;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class Ledger_v1Test {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
