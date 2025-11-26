fca8be25802c96c66c0b2339dd54f79c520fca1c
gui-tr <gtrionfo.dev@pm.me>
2025-11-26 21:44:13 +0000

    configs

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,12 +14,23 @@ repositories {
 dependencies {
     annotationProcessor("io.micronaut:micronaut-http-validation")
     annotationProcessor("io.micronaut.serde:micronaut-serde-processor")
+    annotationProcessor("org.projectlombok:lombok:1.18.42")
+    annotationProcessor("io.micronaut.openapi:micronaut-openapi")
+
     implementation("io.micronaut.serde:micronaut-serde-jackson")
+    implementation("io.swagger.core.v3:swagger-annotations")
+    implementation("io.micronaut.openapi:micronaut-openapi-annotations")
+    implementation("org.slf4j:slf4j-api:1.7.25")
+    implementation("io.swagger.core.v3:swagger-annotations")
+    implementation("io.micronaut.openapi:micronaut-openapi-annotations")
+
     compileOnly("io.micronaut:micronaut-http-client")
+    compileOnly("org.projectlombok:lombok:1.18.42")
+
     runtimeOnly("ch.qos.logback:logback-classic")
+
     testImplementation("io.micronaut:micronaut-http-client")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation("com.h2database:h2:2.4.240")
 }
 
 
@@ -53,11 +64,16 @@ micronaut {
         optimizeNetty = true
         replaceLogbackXml = true
     }
+
 }
 
 
 tasks.named<io.micronaut.gradle.docker.NativeImageDockerfile>("dockerfileNative") {
     jdkVersion = "21"
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.compilerArgs.add("-parameters")
 }
 
 
