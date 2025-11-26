31bb222c73b896e55c3d59006faed92382481f60
gui-tr <gtrionfo.dev@pm.me>
2025-11-26 22:34:17 +0000

    add operations

--- src/main/java/com/app/ledger/LedgerService.java
+++ src/main/java/com/app/ledger/LedgerService.java
@@ -3,17 +3,17 @@ package com.app.ledger;
 import com.app.account.Account;
 import com.app.account.AccountRepository;
 import com.app.exception.LedgerExceptions;
-import com.app.transaction.Currency;
-import com.app.transaction.FxRate;
-import com.app.transaction.TransactionRepository;
+import com.app.transaction.*;
 import io.micronaut.runtime.http.scope.RequestScope;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 import static com.app.ledger.LedgerUtil.generateAccountId;
 import static com.app.ledger.LedgerUtil.getBalance;
+import static com.app.transaction.FxRate.convert;
 
 @Singleton
 public class LedgerService {
@@ -35,6 +35,10 @@ public class LedgerService {
                         .baseCcy(baseCcy)
                         .build());
     }
+
+    // Get all accounts with balance
+    // todo: implement this
+
 
     // Delete an Account
     // - cannot delete an account if balance is positive
@@ -66,11 +70,53 @@ public class LedgerService {
     // Deposit money into Account (in different ccys)
     // - accounts should exist
     // - convert amount into Account ccy
+    public Transaction depositIntoAccount(String accountNo, BigDecimal amount, Currency currency) {
+        final var account = accountRepository.findAll().stream()
+                .filter(acc -> acc.getAccountNo().equals(accountNo))
+                .findFirst()
+                .orElseThrow(() -> new LedgerExceptions.AccountNotFoundException(accountNo));
+
+        final var convertedAmount = convert(currency, account.getBaseCcy(), amount);
+
+        final var transaction = Transaction.builder()
+                .accountNo(account.getAccountNo())
+                .type(Type.DEPOSIT)
+                .amount(convertedAmount)
+                .currency(currency)
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        account.getTransactions().add(transaction);
+        return transaction;
+    }
 
     // Withdraw money from Account
     // - accounts should exist
     // - withdraw only in Account ccy
     // - withdraw only possible if funds available (no overdraft possible)
+    public Transaction withdrawFromAccount(String accountNo, BigDecimal amount) {
+        final var account = accountRepository.findAll().stream()
+                .filter(acc -> acc.getAccountNo().equals(accountNo))
+                .findFirst()
+                .orElseThrow(() -> new LedgerExceptions.AccountNotFoundException(accountNo));
+
+        if (getBalance(account.getTransactions()).subtract(amount).compareTo(BigDecimal.ZERO) < 0) {
+            throw new LedgerExceptions.InsufficientFundsException(accountNo);
+        }
+
+        final var transaction = Transaction.builder()
+                .accountNo(account.getAccountNo())
+                .type(Type.WITHDRAWAL)
+                .amount(amount)
+                .currency(account.getBaseCcy())
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        account.getTransactions().add(transaction);
+        return transaction;
+    }
+
+
 
     // Transfer money from an Account to Another
     // - both accounts should exist
