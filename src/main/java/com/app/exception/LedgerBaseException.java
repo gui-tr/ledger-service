3a5d1362c0dbd46bf1e0ae293c434f38deb7fb1b
gui-tr <gtrionfo.dev@pm.me>
2025-11-26 21:41:49 +0000

    exception handling

---
+++ src/main/java/com/app/exception/LedgerBaseException.java
@@ -0,0 +1,15 @@
+package com.app.exception;
+
+import io.micronaut.http.HttpStatus;
+import lombok.Getter;
+
+@Getter
+public abstract class LedgerBaseException extends RuntimeException {
+    private final HttpStatus status;
+
+    protected LedgerBaseException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
+    }
+}
+
---
+++ src/main/java/com/app/exception/LedgerExceptions.java
@@ -0,0 +1,27 @@
+package com.app.exception;
+
+import io.micronaut.http.HttpStatus;
+
+public class LedgerExceptions {
+
+    public static class AccountNotFoundException extends LedgerBaseException {
+        public AccountNotFoundException(String accountId) {
+            super("Account not found with id: " + accountId, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public static class InsufficientFundsException extends LedgerBaseException {
+        public InsufficientFundsException(String accountId) {
+            super("Insufficient funds for account id: " + accountId, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public static class PositiveBalanceException extends LedgerBaseException {
+        public PositiveBalanceException(String accountId) {
+            super("Cannot delete positive balance for account id: " + accountId +
+                    "Please empty account first.", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+}
